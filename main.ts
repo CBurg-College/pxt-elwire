a552667218bebabcf8bc4ad9e307f5d21ff18c09
CBurg-College <cburgmicro@outlook.com>
2024-10-14 21:26:32 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,42 @@
+//% color="#00CC00" icon="\u2B88"
+//% block="Electric Wire"
+namespace CBurgElWire {
+
+    export enum ElWire {
+        //% block="el-wire 1"
+        //% block.loc.nl="el-wire 1"
+        Wire1,
+        //% block="el-wire 2"
+        //% block.loc.nl="el-wire 2"
+        Wire2
+    }
+
+    //% block="turn %elwire on for %time sec"
+    //% block.loc.nl="doe %elwire %time sec aan"
+    export function showTime(elwire: ElWire, time: number) {
+        let pin1 = (elwire == ElWire.Wire1 ? DigitalPin.P1 : DigitalPin.P2);
+        pins.digitalWritePin(pin1, 1);
+        basic.pause(time * 1000);
+        pins.digitalWritePin(pin1, 0);
+    }
+
+    //% block="turn %elwire on"
+    //% block.loc.nl="doe %elwire aan"
+    export function showOn(elwire: ElWire) {
+        let pin2 = (elwire == ElWire.Wire1 ? DigitalPin.P1 : DigitalPin.P2);
+        pins.digitalWritePin(pin2, 1);
+    }
+
+    //% block="turn %elwire off"
+    //% block.loc.nl="doe %elwire uit"
+    export function showOff(elwire: ElWire) {
+        let pin3 = (elwire == ElWire.Wire1 ? DigitalPin.P1 : DigitalPin.P2);
+        pins.digitalWritePin(pin3, 0);
+    }
+
+    //% block="wait %time sec"
+    //% block.loc.nl="wacht %time sec"
+    export function wait(time: number) {
+        basic.pause(time * 1000);
+    }
+}
